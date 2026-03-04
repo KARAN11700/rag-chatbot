048ea5252302d905b1d977dd4d61130841e9937d
KARAN11700 <55971675+KARAN11700@users.noreply.github.com>
2026-03-04 20:09:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-#print("Hello World from Dockerized Python App 🚀")
+from fastapi import FastAPI
+from pydantic import BaseModel
+from rag_pipeline import get_rag_chain
+
+app = FastAPI()
+qa_chain = get_rag_chain()
+
+class QueryRequest(BaseModel):
+    query: str
+
+@app.post("/chat")
+def chat(request: QueryRequest):
+    response = qa_chain.run(request.query)
+    return {"response": response}
